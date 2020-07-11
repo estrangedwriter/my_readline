f2624881d57751ee2288343eeb6c60f2f626cadc
estrangedwriter <jeromezhang1996@gmail.com>
2020-07-10 23:04:45 -0700

    progress 07/10/2020

--- my_readline.c
+++ my_readline.c
@@ -5,14 +5,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 char* my_readline(int fd);
 
-int main(int ac, char **av)
+int main()
 {
   char *str = NULL;
 
   int fd = open("./file.txt", O_RDONLY);
+
   while ((str = my_readline(fd)) != NULL)
   {
       printf("%s\n", str);
@@ -29,36 +31,29 @@ int main(int ac, char **av)
 
 char* my_readline (int fd) {
 
-  char* buffer = NULL;
-  buffer = malloc (READLINE_READ_SIZE * sizeof(char));
+  char buffer[READLINE_READ_SIZE];
   size_t r;
-  int index;
+  int counter, i;
 
   r = read(fd, buffer, READLINE_READ_SIZE);
+  while (buffer[counter] != '\n') {
+    putchar(buffer[counter]);
+    counter++;
+  }
+  printf("number of bytes read: %ld\nnumber of bytes to newline: %d\n", r, counter);
+  
+  char* buffer2 = NULL;
+  buffer2 = (char*)malloc( (sizeof(char)) * counter + 1);
 
-  for (index = 0; index < r; index++ ) 
-    putchar( buffer[index] );
-  free(buffer);
-  buffer = NULL;  
-  return buffer;
+  for (i = 0; i < counter; i++) {
+    buffer2[i] = buffer[i];
+    putchar(buffer2[i]);
+  }
+  
+  free(buffer2);
+  printf("\n");
+  buffer2 = NULL;  
+  return buffer2;
 }
 
 
-// char* my_readline(int fd) {
-  
-//   FILE* file;
-//   int c;
-//   int index;
-
-//   for (index = 1; index < 5; index++) {
-//     c = getc(file); 
-//     printf("%c", c);
-//   }
-
-//   if (fd == 0) {
-//     return "cool";
-//   }
-//   else if (fd != 0) {
-//     return "wow";
-//   }
-// }
